5efda512bae942cbb5154bff6e558931a6992a70
alopexc0de <tehfoxy.c0de@gmail.com>
2012-10-09 22:02:38 -0400

    Stopped log directory from showing

--- short/getfiles.php
+++ short/getfiles.php
@@ -2,7 +2,7 @@
 if ($handle = opendir('.')) {
    while (false !== ($file = readdir($handle)))
       {
-          if ($file != "." && $file != ".." && $file != "index.php" && $file != "getfiles.php" && $file != "config.php" && $file != "url")
+          if ($file != "." && $file != ".." && $file != "index.php" && $file != "getfiles.php" && $file != "config.php" && $file != "url" && $file != "log")
 	  {
 		$last_modified = filemtime($file); 
 		//print(date("m/j/y h:i:s a", $last_modified)); 
