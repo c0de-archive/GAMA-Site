754befe4b5593f78789cd4a044551411b2bc66c4
alopexc0de <tehfoxy.c0de@gmail.com>
2012-11-16 04:16:01 -0500

    Minor changes for url

--- img/getfiles.php
+++ img/getfiles.php
@@ -1,12 +1,12 @@
 <?php
-if ($handle = opendir('.')) {
+if ($handle = opendir('Pictures')) {
    while (false !== ($file = readdir($handle)))
       {
           if ($file != "." && $file != ".." && $file != "index.php" && $file != "getfiles.php" && $file != "imgup.php" && $file != ".htaccess")
 	  {
 		$last_modified = filemtime($file); 
 		//print(date("m/j/y h:i:s a", $last_modified)); 
-          	$thelist .= '<a href="http://unps-gama.tk/img/?img='.$file.'">'.$file.'</a> Last Modified: <font align="right" color="green">'. date("m/j/y h:i:s a", $last_modified) .'</font><br />';
+          	$thelist .= '<a href="http://img.unps-gama.info/?img='.$file.'">'.$file.'</a> Last Modified: <font align="right" color="green">'.'Not Availiable at the moment...'/*date("m/j/y h:i:s a", $last_modified) */.'</font><br />';
           }
        }
   closedir($handle);
