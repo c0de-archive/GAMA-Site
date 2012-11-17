ec5a2c9049623ac88ff88b982815d13e0406aad6
alopexc0de <tehfoxy.c0de@gmail.com>
2012-11-16 20:12:49 -0500

    Changed links, fixed JS

--- index.php
+++ index.php
@@ -55,17 +55,18 @@
 	</div>
 	<div id="navigation">
 		<ul>
-			<li><a href="javascript:unhide('home');hide('about');hide('contact');hide('minecraft');hide('Privacy');">Home</a></li>
-			<li><a href="javascript:unhide('about');hide('home');hide('contact');hide('minecraft');hide('Privacy');">About</a></li>
+			<li><a href="#" onclick="javascript:unhide('home');hide('about');hide('contact');hide('minecraft');">Home</a></li>
+			<li><a href="#" onclick="javascript:unhide('about');hide('home');hide('contact');hide('minecraft');">About</a></li>
 			<li><a href="http://www.img.unps-gama.info/" target="img">IMGShare</a></li>
 			<li><a href="http://www.unps-gama.info/fp/flatpress/" target="blog">Personal Blog</a></li>
 			<li><a href="http://www.unps-gama.info/upload/index.php" target="upload">Uploads</a></li>
 			<li><a href="http://www.unps-gama.info/pox/" target="pox">Proxy</a></li>
 			<li><a href="https://github.com/alopexc0de/GAMA-Site" target="_ghgama">GitHub</a></li>
-			<li><a href="javascript:unhide('contact');hide('home');hide('about');hide('minecraft');hide('Privacy');">Contact Us</a></li>
+			<li><a href="#" onclick="javascript:unhide('contact');hide('home');hide('about');hide('minecraft');">Contact Us</a></li>
 			<li><a href="http://www.unps-gama.info/short" target="_short">URL Shortner</a></li>
-			<li><a href="javascript:unhide('minecraft');hide('home');hide('about');hide('contact');hide('Privacy');">Minecraft</a></li>
-			<!--<li><a href="javascript:unhide('Privacy');hide('home');hide('about');hide('contact');hide('minecrft')">Privacy Policy</a></li>-->
+			<li><a href="#" onclick="javascript:unhide('minecraft');hide('home');hide('about');hide('contact');">Minecraft</a></li>
+			<li><a href="http://unps-gama.info/privacy.html">Privacy Policy</a></li>
+			<li><a href="http://unps-gama.info/ToS.html">Terms of Service</a></li>
 		</ul>
 	</div>
 	<div id="content-container1">
@@ -88,6 +89,10 @@
 					UnProfessional Standards - GAMATechnologies | Home Page
 				</h2><hr />
 				<p>
+					@Nov 16, 2012 - 1933 EST: <br />
+					Our new updated image host, which can be found at <a href="http://img.unps-gama.info" target="img">IMGShare</a>, is one of our recent changes. Others include a <a href="ToS.html"> Terms of Service</a>, and a <a href="privacy.html">Privacy Policy</a>.
+					</p><hr />
+				<p>
 					@Nov 2, 2012 - 0130 EST: <br />
 					We have officially launched with an actual domain name! In the next few days you'll also see some changes to this site, hopefully for the better. I have been hard at work setting up an email system, and it has caused a lot of grief. If you see webmail one day, then great, I managed to get the mysql working with postfix and whatever I choose to use for webmail. Registration is, as always going to be free. I am also currently adding mysql to other sections of my site, and I'm debating on requiring user login for uploading files and pictures. I have added a privacy policy <a href="javascript:unhide('Privacy');hide('home');hide('about');hide('contact');hide('minecrft')">Here</a>. So have fun with that.
 				</p><hr />
@@ -107,16 +112,9 @@
 					<h2>
 						UnProfessional Standards - GAMATechnologies | Contact Us
 					</h2><hr />
-					<p>Hello, I'm assuming that you want to contact me.</p><p>Here's a list of ways (listed in order of highest chance of reaching me to lowest):</p>
-					<ul>
+					<p>Hello, I'm assuming that you want to contact me.</p><ul>
 						<li>Skype: <a href="skype:alopexlagopus-c0de?chat">alopexlagopus-c0de</a></li>
-						<li>Facebook: <a href="http://facebook.com/G1itch3D" target="_fbgama">G1itch3D</a></li>
-						<li>Email: <a href="mailto:c0de@unps-gama.info">c0de@unps-gama.tk</a></li>
-						<li>TeamSpeak: <a href="teamspeak:unps-gama.info">unps-gama.tk</a> default channel</li>
 					</ul>
-				</div>
-				<div id="Privacy" class="hidden">
-					<!-- START PRIVACY POLICY CODE --><div style="font-family: verdana;"><strong>What information do we collect?</strong> <br /><br />We collect information from you when you register on our site, fill out a form or store files.  <br /><br />When ordering or registering on our site, as appropriate, you may be asked to enter your: name or e-mail address. You may, however, visit our site anonymously.<br /><br />Google, as a third party vendor, uses cookies to serve ads on your site. Google's use of the DART cookie enables it to serve ads to your users based on their visit to your sites and other sites on the Internet. Users may opt out of the use of the DART cookie by visiting the Google ad and content network privacy policy..<br /><br /><strong>What do we use your information for?</strong> <br /><br />Any of the information we collect from you may be used in one of the following ways: <br /><br />; To personalize your experience<br />(your information helps us to better respond to your individual needs)<br /><br /><br />; To administer a contest, promotion, survey or other site feature<br /><br /><br /><br /><strong>How do we protect your information?</strong> <br /><br />We implement a variety of security measures to maintain the safety of your personal information when you enter, submit, or access your personal information. <br /><br /><strong>Do we use cookies?</strong> <br /><br />Yes (Cookies are small files that a site or its service provider transfers to your computers hard drive through your Web browser (if you allow) that enables the sites or service providers systems to recognize your browser and capture and remember certain information<br /><br /> We use cookies to understand and save your preferences for future visits, keep track of advertisements and compile aggregate data about site traffic and site interaction so that we can offer better site experiences and tools in the future.<br /><br /><strong>Do we disclose any information to outside parties?</strong> <br /><br />We do not sell, trade, or otherwise transfer to outside parties your personally identifiable information. This does not include trusted third parties who assist us in operating our website, conducting our business, or servicing you, so long as those parties agree to keep this information confidential. We may also release your information when we believe release is appropriate to comply with the law, enforce our site policies, or protect ours or others rights, property, or safety. However, non-personally identifiable visitor information may be provided to other parties for marketing, advertising, or other uses.<br /><br /><strong>Third party links</strong> <br /><br /> Occasionally, at our discretion, we may include or offer third party products or services on our website. These third party sites have separate and independent privacy policies. We therefore have no responsibility or liability for the content and activities of these linked sites. Nonetheless, we seek to protect the integrity of our site and welcome any feedback about these sites.<br /><br /><strong>California Online Privacy Protection Act Compliance</strong><br /><br />Because we value your privacy we have taken the necessary precautions to be in compliance with the California Online Privacy Protection Act. We therefore will not distribute your personal information to outside parties without your consent.<br /><br /><strong>Childrens Online Privacy Protection Act Compliance</strong> <br /><br />We are in compliance with the requirements of COPPA (Childrens Online Privacy Protection Act), we do not collect any information from anyone under 13 years of age. Our website, products and services are all directed to people who are at least 13 years old or older.<br /><br /><strong>Online Privacy Policy Only</strong> <br /><br />This online privacy policy applies only to information collected through our website and not to information collected offline.<br /><br /><strong>Your Consent</strong> <br /><br />By using our site, you consent to our privacy policy.<br /><br /><strong>Changes to our Privacy Policy</strong> <br /><br />If we decide to change our privacy policy, we will post those changes on this page, and/or update the Privacy Policy modification date below. <br /><br />This policy was last modified on November 2nd, 2012<br /><br /><strong>Contacting Us</strong> <br /><br />If there are any questions regarding this privacy policy you may contact us using the information below. <br /><br />http://unps-gama.info<br />c0de@unps-gama.info</div><!-- END PRIVACY POLICY CODE -->
 				</div>
 				<div id="minecraft" class="hidden">
 					<h4>Server Status:</h4><br />
@@ -156,60 +154,7 @@
 						<p>Direct: <input type="text" size="28" name="directlink" value="http://www.unps-gama.info/mcimage.php" readonly="readonly" /></p>
 						<p>HTML: <input type="text" size="88" name="html" value="&lt;img src='http://www.unps-gama.info/mcimage.php alt='UnPS-GAMA Minecraft server status image' &#47;&gt;" readonly="readonly" /></p>
 						<p>BBCode (for forums): <input type="text" size="58" value="[align=center][img]http://www.unps-gama.info/mcimage.php[/img][/align]" readonly="readonly" name="bbcode" /></p></form></td></tr>
-					</table><br /><hr /><br />
-					<!--<?php/*
-						$choice = '';
-						$map = '';
-						$uniqueid = '';
-						$voterip = '';
-						$random = rand(); 
-						if(!$_POST['submit'])
-						{
-					?>
-					<h4>Poll - I'm planning on doing a custom map for a short time, who votes yes, who votes no and if you're yes tell me what map please</h4>
-					<form action="index.php" method="post">
-						<p>Yes, put up a custom map for a short time <input type="radio" name="choice" value="yes" /></p>
-						<p>No, don't change the map <input type="radio" name="choice" value="no" /></p>
-						<p>Map to use(optional): <input name="map" type="text" size="30" value="No map was supplied" /></p>
-						<input type="hidden" name="uniqueid" value="<?php echo $random; ?>" />
-						<input type="hidden" name="voterip" value="<?php echo $_SERVER['REMOTE_ADDR']; ?>" />
-						<input type="submit" name="submit" value="submit" />
-					</form>
-					<?php
-						}
-						$choice = $_POST['choice'];
-						//$map = $_POST['map'];
-						$uniqueid = $_POST['uniqueid'];
-						$voterip = $_POST['voterip'];
-						if($_POST["submit"])
-						{
-							if(isset($_POST['choice']) && isset($_POST['uniqueid']) && isset($_POST['voterip']) && $choice != "" && $uniqueid != "" && $voterip != "") 
-							{
-								if(isset($_POST['map'])){ 
-									$map = $_POST['map'];
-								}else{
-									$map = "No Map was supplied";
-								}
-								$myFile = "poll.txt";
-								$fh = fopen($myFile, 'a') or die("can't open file");
-								$stringData = "Voter: ".$uniqueid." Of IP: ".$voterip.", has voted ".$choice." and their map is: ".$map.".\n";
-								fwrite($fh, $stringData);
-								fclose($fh);	
-								echo "
-								<script type='text/javascript'>
-									unhide('minecraft');hide('home');hide('about');hide('contact');
-								</script>
-								<h3>Thanks for voting!</h3>";
-								//echo "Voter: ".$uniqueid." Of IP: ".$voterip.", has voted ".$choice." and their map is: ".$map."\r\n\r\n";
-							}if($choice == "" || $uniqueid == "" || $voterip == ""){
-								echo "
-								<script type='text/javascript'>
-									unhide('minecraft');hide('home');hide('about');hide('contact');
-								</script>
-								<h1>Something's not right here... Try using the poll again</h1>";
-							}
-						}
-					*/?>-->
+					</table><br /><hr />
 				</div>
 			</div>
 			<div id="aside">
@@ -244,9 +189,6 @@
      				</form>
 				<br /><h3>Files currently on GAMA: </h3>
       				<?php 
-      					//include 'preg_find.php';
-						//$files = preg_find('/./', './upload', PREG_FIND_RECURSIVE);
-						//foreach($files as $file) printf("<br />%s\n", $file); 
 						include 'upload-list.php';
 						getDirectory( "./upload" ); 
 				?>
