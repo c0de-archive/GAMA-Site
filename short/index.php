4f2fd52fe48ec5ce21dcd144744d915a4994f453
alopexc0de <tehfoxy.c0de@gmail.com>
2012-12-24 02:30:02 -0500

    Cleaned up source + commented all php + HTML4 clean

--- short/index.php
+++ short/index.php
@@ -1,132 +1,140 @@
-<?php 
-require('dbsettings.php'); 
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+	<title>URL Shortner</title>
+	<meta http-equiv="content-type" content="text/html;charset=utf-8" />
+	<meta name="description" content="Link shortener for UnProfessional Standards" />
+	<meta name="keywords" content="GAMA,UnPS,upstandards,unps-gama,gama-unps,unps,gama,davitech,davitodd" />
+	<meta name="author" content="David Todd" />
+	<link rel="shortcut icon" type="image/ico" href="http://unps-gama.info/favicon.ico" />
+	<link rel="shortcut icon" type="image/x-icon" href="http://unps-gama.info/favicon.ico" />
+	
+	<style type="text/css">
+		#content{ 
+			text-align: center;
+			background: #000;
+			color: #090;
+			padding: 5px;
+			float: center;
+		}
+	</style>
+</head>
 
-function input($input){
-	if ($input == null) die("No Input Provided, Aborting\r\n<br>");
+<?php 
+require('dbsettings.php'); // Gotta connect to dem databases
+
+function input($input){ // Cleans input to be used in mysql
+	if ($input == null) die("No Input Provided, Aborting\r\n<br />");
 	$input = mysql_real_escape_string($input);
 	return $input;
 }
 
-function prepOutputText($text) {
-	if ($text == null) die("No Input Provided, Aborting\r\n<br>");
+function prepOutputText($text) { // Takes mysql string and makes it able to be used
+	if ($text == null) die("No Input Provided, Aborting\r\n<br />");
 	$output = htmlentities(stripslashes($text),ENT_QUOTES);
 	return $output;
 }
 
-/*function is_available($url, $timeout = 30) {
-	$ch = curl_init(); // get cURL handle
-	$opts = array(CURLOPT_RETURNTRANSFER => true, // do not output to browser
-				  CURLOPT_URL => $url,            // set URL
-				  CURLOPT_NOBODY => true, 		  // do a HEAD request only
-				  CURLOPT_TIMEOUT => $timeout);   // set timeout
-	curl_setopt_array($ch, $opts); 
-	curl_exec($ch); // do it!
-	$retval = curl_getinfo($ch, CURLINFO_HTTP_CODE) == 200; // check if HTTP OK
-	curl_close($ch); // close handle
-	return $retval;
-}*/
+function getServerStatus($ip, $port = 80) { // Tests if host is online or not
+	return @fsockopen($ip, $port, $errnum, $errstr) == false ? 'offline' : 'online';
+} 
 
-function GetServerStatus($site, $port){
-	$status = array("OFFLINE", "ONLINE");
-	$fp = @fsockopen($site, $port, $errno, $errstr, 2);
-	if (!$fp){
-		return $status[0];
-	} else{ 
-		return $status[1];
-	}
-}
+$submit; // Declare this since my logs are annoying me to no end x.x
 
-$submit;
-
-if(isset($_GET['l'])) {
-	$l = $_GET['l'];
-	$l = input($l);
-	$sql = "SELECT id, link, shortlink FROM $tbl_name WHERE shortlink='$l'";
-	$result = mysql_query($sql);
-	$count = mysql_num_rows($result);
-	if($count == 1){
-		while ($row = mysql_fetch_assoc($result)){ // Attempt to pull all data concerning that one user from table	
-			$id = $row['id'];
-			$link = $row['link'];
-			$short = $row['shortlink'];
-			mysql_close();
-			echo "Redirecting you to your site, please wait...";
-			$link = prepOutputText($link);
-			header("location:".$link);
+if(isset($_GET['l'])) { // if there is a link...
+	$l = $_GET['l']; // Bring the link into a variable
+	$l = input($l); // Clean said variable to be used in mysql
+	$sql = "SELECT id, link, shortlink FROM $tbl_name WHERE shortlink='$l'"; // Find the link in the table
+	$result = mysql_query($sql); // mysql stuff
+	$count = mysql_num_rows($result); // How many rows were found?
+	if($count == 1){ // There should only be one row
+		while ($row = mysql_fetch_assoc($result)){ // Start pulling data from the row
+			$id = $row['id']; // Lets get the id - not used, just a number used to identify the link or something, it's unique
+			$link = $row['link']; // This is the full link
+			$short = $row['shortlink']; // This is the short link - Should be a unique number
+			mysql_close(); // close the mysql connection
+			echo "Redirecting you to your site, please wait..."; // never seen
+			$link = prepOutputText($link); // make the link work
+			header("location:".$link); // redirect right away
 		}	
 	}else{
-		echo "Hmmm... It appears that your link doesn't exist in my database. Try again?";
-		header("location:http://unps.us/");
+		echo "Hmmm... It appears that your link doesn't exist in my database. Try again?"; // not seen
+		header("location:http://unps.us/"); // redirect right away
 	}
 }
 ?>
-<title>URL Shortner</title>
-<link rel="shortcut icon" type="image/ico" href="http://unps-gama.info/favicon.ico" />
-<link rel="shortcut icon" type="image/x-icon" href="http://unps-gama.info/favicon.ico" />
-<body bgcolor="black" text="greem"><div align="center">
-<img src="http://unps-gama.info/upload/Pictures/header.png"><br>
-<h4>Welcome to the UnPS-GAMA link shortner</h4><hr>
-<p>All you gotta do is put a link into the box and click submit</p>
-<?php
-if(!$_POST['submit']){
-?>
-<form id="short" action="index.php" method="POST" >
-<p>Destination:<br><input name="dest" id="dest" class="dest" title="Insert URL here" placeholder="Insert URL here" value="" type="text" size="30" ></p>
-<input type="submit" name="submit" value="submit">
-</form>
 
-<?php
-	}
-if($_POST["submit"]){
-	if(isset($_POST['dest'])) {
-		$dest=$_POST['dest'];
-		$dest = input($dest);
-		$sql = "SELECT id, link, shortlink FROM $tbl_name WHERE link='$dest'";
-		$result = mysql_query($sql);
-		$count = mysql_num_rows($result);
-		if($count == 1){
-			while ($row = mysql_fetch_assoc($result)){ // Attempt to pull all data concerning that one user from table	
-				$id = $row['id'];
-				$link = $row['link'];
-				$short = $row['shortlink'];
-				echo "From what I can tell, this particular link was already shortened before.<br>Here's the link: <a href=\"http://unps.us/?l=$short\">http://unps.us/?l=$short</a>";
-			}	
-		}else{
-			if (strpos($dest, 'http://') === false) {
-				if (strpos($dest, 'https://') === false){
-					$ip = gethostbyname($dest);
-					$dest = 'http://'.$dest;
+<body>
+	<div class="content" id="content">
+		<img src="http://unps-gama.info/upload/Pictures/header.png" alt="Header image" /><br />
+		<h4>Welcome to the UnPS-GAMA link shortner</h4><hr />
+		<p>All you gotta do is put a link into the box and click submit</p>
+	
+		<?php
+			if(!$_POST['submit']){ // If the submit button was not pressed show the form
+		?>
+	
+		<form id="short" title="short" action="index.php" method="POST" >
+			<p>Destination:<br />
+			<input name="dest" id="dest" class="dest" title="Insert URL here" placeholder="Insert URL here" value="" type="text" size="30" /></p>
+			<input type="submit" name="submit" value="submit" />
+		</form>
+
+		<?php
+			}
+			if($_POST["submit"]){ // If submit button was pressed...
+				if(isset($_POST['dest'])) { // If the text box has something in it
+					$dest=$_POST['dest']; // Pull that into a variable
+					$dest = input($dest); // Clean the variable for mysql
+					$sql = "SELECT id, link, shortlink FROM $tbl_name WHERE link='$dest'"; // Check the table if the link was posted before
+					$result = mysql_query($sql); // mysql stuff
+					$count = mysql_num_rows($result); // Count the rows returned, if any
+					if($count == 1){ // If a row is returned, there should only be one
+						while ($row = mysql_fetch_assoc($result)){ // Pull all the data from the returned row
+							$id = $row['id']; // Lets get the id - not used, just a number used to identify the link or something, it's unique
+							$link = $row['link']; // This is the full link
+							$short = $row['shortlink']; // This is the short link - Should be a unique number
+							echo "From what I can tell, this particular link was already shortened before.<br>Here's the link: <a href=\"http://unps.us/?l=$short\" target=\"$short\">http://unps.us/?l=$short</a>"; // Return link that's already in table
+						}	
+					}else{ // If no row is returned, it is assumed that the link isn't there
+						if (strpos(strtolower($dest), 'http://') === false) { // Simple test to see if http:// not part of the link
+							if (strpos(strtolower($dest), 'https://') === false){ // Simple test to see if https:// not part of the link passing the above check
+								$ip = gethostbyname($dest); // Get IP address of link
+								$dest = 'http://'.$dest; // Add http:// to link 
+							}
+						}
+						if (strpos(strtolower($dest), 'http://') !== false) { // Simple test to see if http:// is part of the link
+							if (strpos(strtolower($dest), 'https://') !== false){ // Simple test to see if https:// is part of the link failing the above check
+								$dest = str_replace("https://", "", $dest); // If https:// exists, remove it
+								$ip = gethostbyname($dest); // Get IP address of link
+								$dest = 'https://'.$dest; // Re-add https:// to link
+							}
+							$dest = str_replace("http://", "", $dest); // if http:// exists, remove it
+							$ip = gethostbyname($dest); // Get IP address of link
+							$dest = 'http://'.$dest; // Re-add http:// to link
+						}
+						if(GetServerStatus($ip, 80) != "online") die("Hmmm it seems that your link is dead.\r\nPlease try again"); // Check to see if the host is alive
+						$short = substr(number_format(time() * rand(),0,'',''),0,10); // Create a random number 10 digits long
+						$short = base_convert($short, 10, 36); // Convert the 10 digit random number into Base36
+						$sql="INSERT INTO $tbl_name (link, shortlink) VALUES ('$dest', '$short')"; // Try to add the link and short link into table
+						$result=mysql_query($sql); // mysql stuff
+						if($result){ // If the link is added to the table
+							echo "It appears that I have succeded in making a short link.<br>You'll find it here: <a href=\"http://unps.us/?l=$short\" target=\"$short\">http://unps.us/?l=$short</a> ";
+						}else { // If the link is not added to the table
+							echo "There was a problem trying to register your link - Could be a database error";
+						}
+					}
 				}
-			}
-			if (strpos($dest, 'http://') !== false) {
-				if (strpos($dest, 'https://') !== false){
-					$dest = str_replace("https://", "", $dest);
-					$ip = gethostbyname($dest);
-					$dest = 'https://'.$dest;
+				if(!$dest){ // If the textbox was empty when loaded
+						echo ' 
+							<p>Sorry, you are not able to shorten something without a url<br /></p>\r\n
+							<a href="http://unps.us">Back to index</a><br />\r\n
+							<a href="http://unps-gama.info">Home</a>
+						';	
+					}
 				}
-				$dest = str_replace("http://", "", $dest);
-				$ip = gethostbyname($dest);
-				$dest = 'http://'.$dest;
-			}
-			if(GetServerStatus($ip, 80) != "ONLINE") die("Hmmm it seems that your link is dead.\r\nPlease try again");
-			$short = substr(number_format(time() * rand(),0,'',''),0,10);
-			$short = base_convert($short, 10, 36); 
-			$sql="INSERT INTO $tbl_name (link, shortlink) VALUES ('$dest', '$short')";
-			$result=mysql_query($sql);
-			if($result){
-				echo "It appears that I have succeded in making a short link.<br>You'll find it here: <a href=\"http://unps.us/?l=$short\" target=\"$short\">http://unps.us/?l=$short</a> ";
-			}else {
-				echo "There was a problem trying to register your link - Could be a database error";
-			}
-		}
-	}
-	if(!$dest){
-			echo '
-				Sorry, you are not able to shorten something without a url <br>
-				<a href="http://unps.us">Back to index</a><br>
-				<a href="http://unps-gama.info">Home</a>
-			';	
-		}
-}
-mysql_close();
-?>
+		mysql_close(); // Close any mysql connections still open
+		?>
+	</div>
+</body>
+</html>
