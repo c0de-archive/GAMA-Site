a34d45defc884fffb86c9c4dcc72dd35d130a9c7
alopexc0de <davitodd@gmail.com>
2013-02-15 18:14:58 -0500

    Function for generating thumbnails on upload

---
+++ img/helper.genthumb.php
@@ -0,0 +1,15 @@
+<?php
+
+	/*------------------------------------------
+	 * Helper.GenThumb.php - Function for generating thumbnails on upload
+	 * 
+	 * Copyright (c) 2013 David Todd(c0de) of http://www.unps-gama.info and http://unps.us 
+	 * for use with the image host (http://img.unps-gama.info)
+	 *------------------------------------------
+	 */
+
+	function genthumb($input){
+		echo "Placeholder for automatic 100x100px thumbnail generation of new pictures<br />\n";
+	}
+
+?>
