eb52aaf7d4863850c21223a518a2cd7e3b793ecf
alopexc0de <davitodd@gmail.com>
2013-02-18 22:51:55 -0500

    createThumbs() added to rebuild all thumbnails

--- img/helper.genthumb.php
+++ img/helper.genthumb.php
@@ -8,7 +8,7 @@
 	 *------------------------------------------
 	 */
 
-	function genthumb($imageName){
+	function genthumb($imageName){ // Generates one thumbnail of one picture on demand
 		//echo "Placeholder for automatic 100x100px thumbnail generation of new pictures<br />\n";
 		$nw = 100;
 		$nh = 100;
@@ -22,7 +22,7 @@
 		$w = $size[0];
 		$h = $size[1];
 		
-		switch($stype) {
+		switch(strtolower($stype)) {
 			case 'gif':
 				$simg = imagecreatefromgif($source);
 				break;
@@ -40,7 +40,7 @@
 		$h_height = $nh/2;
 		$w_height = $nw/2;
  
-		if($w> $h){
+		if($w> $h){ // Debating weather or not to keep the cropping
 			$adjusted_width = $w / $hm;
 			$half_width = $adjusted_width / 2;
 			$int_width = $half_width - $w_height;
@@ -54,7 +54,7 @@
 			imagecopyresampled($dimg,$simg,0,0,0,0,$nw,$nh,$w,$h); 
 		}   
 
-		switch($stype) {
+		switch(strtolower($stype)) {
 			case 'gif':
 				imagegif($dimg,$dest,100);
 				break;
@@ -67,5 +67,75 @@
 		}
 		//imagejpeg($dimg,$dest,100);
 	}
+	
+	function createThumbs(){ // Generates a thumbnail for every image in the Pictures directory
+	$pathToImages = 'Pictures/';
+	$pathToThumbs = 'thumbs/';
+	$dir = opendir( $pathToImages );
+	while (false !== ($fname = readdir( $dir ))) {
+		$info = pathinfo($pathToImages . $fname);
+		if ($fname == "." || $fname == "..") continue;
+		$nw = 100;
+		$nh = 100;
+		$source = $pathToImages.$fname;
+
+		$stype = explode(".", $source);
+		$stype = $stype[count($stype)-1];	
+
+		echo "Creating thumbnail for {$fname}..";
+		
+		$size = getimagesize($source);
+		$w = $size[0];
+		$h = $size[1];
+		
+		switch(strtolower($stype)) {
+			case 'gif':
+				$simg = imagecreatefromgif($source);
+				break;
+			case 'jpg':
+				$simg = imagecreatefromjpeg($source);
+				break;
+			case 'png':
+				$simg = imagecreatefrompng($source);
+				break;
+		}
+			
+		$dimg = imagecreatetruecolor($nw, $nh);
+		$wm = $w/$nw;
+		$hm = $h/$nh;
+		$h_height = $nh/2;
+		$w_height = $nw/2;
+ 
+		if($w> $h){ // Debating weather or not to keep the cropping
+			$adjusted_width = $w / $hm;
+			$half_width = $adjusted_width / 2;
+			$int_width = $half_width - $w_height;
+			imagecopyresampled($dimg,$simg,-$int_width,0,0,0,$adjusted_width,$nh,$w,$h);
+		}elseif(($w <$h) || ($w == $h)){     
+			$adjusted_height = $h / $wm;     
+			$half_height = $adjusted_height / 2;     
+			$int_height = $half_height - $h_height;         
+			imagecopyresampled($dimg,$simg,0,-$int_height,0,0,$nw,$adjusted_height,$w,$h); 
+		}else{     
+			imagecopyresampled($dimg,$simg,0,0,0,0,$nw,$nh,$w,$h); 
+		}   
+
+		switch(strtolower($stype)) {
+			case 'gif':
+				imagegif($dimg, "{$pathToThumbs}{$fname}" ,100);
+				echo ". Done. <img src='C:\\users\\c0de\\Desktop\\thumbs\\$fname'><br>\n";
+				break;
+			case 'jpg':
+				imagejpeg($dimg, "{$pathToThumbs}{$fname}" ,100);
+				echo ". Done. <img src='C:\\users\\c0de\\Desktop\\thumbs\\$fname'><br>\n";
+				break;
+			case 'png':
+				imagepng($dimg, "{$pathToThumbs}{$fname}" ,9);
+				echo ". Done. <img src='C:\\users\\c0de\\Desktop\\thumbs\\$fname'><br>\n";
+				break;
+		}
+	}
+	closedir( $dir );
+}
 
 ?>
